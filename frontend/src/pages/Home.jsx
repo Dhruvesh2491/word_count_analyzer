6410cd1d99b37036b841b381e62d5092ba6903c6
Devil2491 <godavale2491@gmail.com>
2025-02-18 12:34:41 +0530

    UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <Container className="py-4">
       <h1 className="mb-4">Webpage Scraper</h1>
       <URLInput />
-      <Card className="mt-4" style={{ width: "70vw",height:"100vh" }}>
+      <Card className="mt-4" style={{ width: "70vw",height:"100%" }}>
         <h2 className="text-center">Results</h2>
         <InsightTable />
       </Card>
